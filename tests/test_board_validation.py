a2b35a54bc0c22530253ab51384640a6103ad0b8
lukhen <lukasz.hen@gmail.com>
2019-08-26 16:00:00 +0200

    Confirmed we can validate a solved board

--- tests/test_board_validation.py
+++ tests/test_board_validation.py
@@ -31,3 +31,18 @@ def test_almost_empty_board():
                 B, B, B, B, B, B, B, B, B]
     # fmt: on
     assert is_valid(empty_bd) is True
+
+
+def test_properly_solved_board():
+    # fmt: off
+    bd_solved = [3, 9, 6, 2, 7, 8, 4, 5, 1,
+                 4, 8, 5, 9, 1, 3, 7, 2, 6,
+                 2, 7, 1, 6, 4, 5, 8, 3, 9,
+                 5, 4, 7, 8, 2, 9, 6, 1, 3,
+                 8, 1, 2, 7, 3, 6, 5, 9, 4,
+                 6, 3, 9, 4, 5, 1, 2, 8, 7,
+                 1, 5, 8, 3, 6, 4, 9, 7, 2,
+                 7, 6, 3, 5, 9, 2, 1, 4, 8,
+                 9, 2, 4, 1, 8, 7, 3, 6, 5]
+    # fmt: on
+    assert is_valid(bd_solved) is True
