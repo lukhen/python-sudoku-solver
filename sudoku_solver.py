f5b170cbb8e76873e4a7057a3cca4efa91981d53
lukhen <lukasz.hen@gmail.com>
2019-08-26 15:48:55 +0200

    Added some data definitions and named a variable better.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,12 +1,24 @@
-def is_valid(s):
-    if s[0] == 1:
+# Data definitions:
+# Board: listOf Squares, length - 81
+# Square: oneOf:
+#          - Value
+#          - None
+# Value: Integer[0, 9]
+
+
+def is_valid(bd: "Board") -> bool:
+    if bd[0] == 1:
         return False
     else:
         return True
 
 
-def solve(s):
-    if is_valid(s):
-        return s
+def solve(bd: "Board") -> "Board" or False:
+    """
+    Produce board with all squares filled,
+    or False if it's unsolvable
+    """
+    if is_valid(bd):
+        return bd
     else:
         return False
