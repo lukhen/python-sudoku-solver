bdc9cb7cf7cde25ba44d306278a9e6a942a467ca
lukhen <lukasz.hen@gmail.com>
2019-08-26 15:10:03 +0200

    Now we can solve any board which is already solved.

---
+++ tests/test_solving.py
@@ -0,0 +1,16 @@
+from sudoku_solver import solve
+
+
+def test_already_solved():
+    # fmt: off
+    bd_solved = [3, 9, 6, 2, 7, 8, 4, 5, 1,
+                 4, 8, 5, 9, 1, 3, 7, 2, 6,
+                 2, 7, 1, 6, 4, 5, 8, 3, 9,
+                 5, 4, 7, 8, 2, 9, 6, 1, 3,
+                 8, 1, 2, 7, 3, 6, 5, 9, 4,
+                 6, 3, 9, 4, 5, 1, 2, 8, 7,
+                 1, 5, 8, 3, 6, 4, 9, 7, 2,
+                 7, 6, 3, 5, 9, 2, 1, 4, 8,
+                 9, 2, 4, 1, 8, 7, 3, 6, 5]
+    # fmt: on
+    assert solve(bd_solved) == bd_solved
