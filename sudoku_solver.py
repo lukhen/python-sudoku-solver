96541ce06fbc69f5ca937df0ec29aa9444e27a1a
lukhen <lukasz.hen@gmail.com>
2019-08-27 09:50:30 +0200

    Extracted a separate behavior.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -9,14 +9,16 @@
 def is_empty(bd: "Board") -> bool:
     return not any(bd)
 
+def has_duplicates(row):
+    first_row_without_blanks = [sq for sq in row if sq is not None]
+    return len(first_row_without_blanks) != len(set(first_row_without_blanks))
 
 def is_valid(bd: "Board") -> bool:
     if is_empty(bd):
         return True
 
     first_row = [bd[0], bd[1], bd[2], bd[3], bd[4], bd[5], bd[6], bd[7], bd[8]]
-    first_row_without_blanks = [sq for sq in first_row if sq is not None]
-    if len(first_row_without_blanks) != len(set(first_row_without_blanks)):
+    if has_duplicates(first_row):
         return False
     else:
         return True
